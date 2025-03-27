75ce2a1f60217ec0d72b5a1ce7b638d3935b51ab
cayeronwi <kate200@bu.edu>
2025-03-27 23:08:01 +0000

    app edits commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+
+function Navbar(){
+
+    return(
+        <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+            <a className="nav-band">
+            <img src="./logo.png"  width="30" height="24" class="d-inline-block align-text-top p-1" alt="" />
+            Group 5: GNU Image Online Extension Repository
+
+            </a>
+
+        </div>
+        
+        </nav>
+        
+    )
+}
+export default Navbar;
