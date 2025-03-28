c9c43cfdf1c6781149ca7a4cdb8a353ba3fc1147
cayeronwi <kate200@bu.edu>
2025-03-28 01:53:32 +0000

    navbar commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,8 +7,8 @@ function Navbar(){
     return(
         <nav className="navbar bg-body-tertiary">
         <div className="container-fluid">    
-            <img src="./logo.png"  width="30" height="24" class="d-inline-block align-text-top" alt="" />
-            <span>Group 5: GNU Image Online Extension Repository</span>
+            <img src="./logo.png"  width="30" height="24" class="d-inline-block align-text-top" alt=""/>
+            <span className="fw-bold">Group 5: GNU Image Online Extension Repository</span>
         </div>
         
         </nav>
