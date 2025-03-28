89994056c5d4c9e3f31758b66d0486f22e1168d3
cayeronwi <kate200@bu.edu>
2025-03-28 02:48:10 +0000

    Register pg commit

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,13 +1,78 @@
-import React from 'react'
+import {useState} from 'react'
+import {Link, useNavigate } from "react-router-dom";
+import axios from 'axios'
 
 
 function Register(){
 
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+
+    const registerUser = async (e) =>{
+        e.preventDefault();
+        axios.post('#', {name, email, password})
+        .then(result => {console.log(result)
+            navigate('#')
+        })
+        .catch(err => console.log(err))
+        
+    }
+
     return(
-        <div>
-            <h2>Regsiter</h2>
+        <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+        <div className="bg-white p-3 rounded w-25">
+            <h2>Register</h2>
+            <form onSubmit={registerUser}>
+                <div className="mb-3">
+                    <label htmlFor="email">
+                        <strong>Name</strong>
+                    </label>
+                    <input
+                    type= "text"
+                    placeholder="name..."
+                    autoComplete="off"
+                    name="email"
+                    className="form-control rounded-0"
+                    onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email">
+                        <strong>Email</strong>
+                    </label>
+                    <input
+                    type= "email"
+                    placeholder="email..."
+                    autoComplete="off"
+                    name="email"
+                    className="form-control rounded-0"
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email">
+                        <strong>Password</strong>
+                    </label>
+                    <input
+                    type= "password"
+                    placeholder="password..."
+                    autoComplete="off"
+                    name="email"
+                    className="form-control rounded-0"
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>             
+                <button type="submit" className="btn btn-success w-100 rounded">Register</button>
+                </form>
+                <p>Already Have an Account?</p>
+                <Link to="/login" className="btn btn-default boarder w-100 bg-light rounded-0 text-decoration-none">Login</Link>
+            
         </div>
-    )
+    </div>   
+    );
 }
 
 export default Register;
