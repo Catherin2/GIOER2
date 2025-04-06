4eb55f8cdbe431d5db2f9bf7339183e8bd8146a5
mmallic <madhumita077@gmail.com>
2025-04-05 21:31:18 -0400

    Create Search.jsx
    Created Search - Initial Commit

---
+++ client/src/Search.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import axios from 'axios';
+
+const Search = () => {
+    const [id, setID] = useState('');
+    const [extension, setExtension] = useState(null);
+
+    const handleSearch = () => {
+        axios.get(`/api/extensions/${id}`)
+        .then(response => setExtension(response.data))
+        .catch(error => console.error('Error fetching extension:', error));
+    };
+
+    return(
+        <div className="container">
+            <h2>Search Service</h2>
+            <button className="btn btn-secondary mb-3" onClick={() => window.history.back()}>Back to dashboard</button>
+            <div>
+                <input
+                type="text"
+                placeholder="Enter Extension ID"
+                value={id}
+                onChange={(e) => setID(e.target.value)}
+                className="form-control mb-2" />
+
+                <button className="btn btn-primary" onClick={handleSearch}>Fetch Extension</button>
+            </div>
+            {extension && (
+                <div className="mt-3">
+                    <h4>Extension Details:</h4>
+                    <p>ID: {extension.id}</p>
+                    <p>Title: {extension.title}</p>
+                    <p>Description: {extension.description}</p>
+                    <p>Category: {extension.category}</p>
+                    <p>Author: {extension.author}</p>
+                    <p>Upload Date: {extension.uploaddate}</p>
+                    <p>rating: {extension.rating}</p>
+                    <p>Archived: {extension.archived ? 'yes' : 'No'}</p>
+                </div>
+            )}
+        </div>
+
+    );
+      
+};
+
+export default Search;
