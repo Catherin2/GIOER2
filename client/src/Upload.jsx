df06858f8e01dad1f4ee7d2bdc6a7f29ee481f96
Catherine <cayeronwi@gmail.com>
2025-04-14 20:18:09 +0000

    upload commit 1402

--- client/src/Upload.jsx
+++ client/src/Upload.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {Container, Row, Col, Button, Card, Navbar, Form} from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
