61babbb5f0eebb263b9e7102a50d0d83fda5d1c2
Catherine <cayeronwi@gmail.com>
2025-04-17 15:23:23 +0000

    app commit 1709

--- client/src/Creation.jsx
+++ client/src/Creation.jsx
@@ -42,7 +42,7 @@ function Creation(){
                     <Card >
                         <Card.Body>
                             <Button variant="secondary" href="/" className="mb-2 w-100">Dashboard</Button>
-                            <Button variant="secondary" href="register" className="mb-2 w-100">SignUp/Login</Button>
+                            <Button variant="secondary" href="/register" className="mb-2 w-100">SignUp/Login</Button>
                        </Card.Body>    
                     </Card>
                 </Col>
