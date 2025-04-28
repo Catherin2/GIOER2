5078b6b0130d0b76ebad8a667cad449582d95d16
Catherine <cayeronwi@gmail.com>
2025-04-28 17:20:57 +0000

    git commit -S -m "api commit28-02"

--- client/src/Api.jsx
+++ client/src/Api.jsx
@@ -1,1 +1,2 @@
 export const SERVER_URL = "https://gioer-cfc6bkewatd5angv.canadacentral-01.azurewebsites.net/api";
+export const SERVER_URL2 = "https://nestjs-g2fpc8bchsf0gyhy.canadacentral-01.azurewebsites.net/api";
